58b32b15b4f6fc62133749082fdbeb7b731be9ec
Benjina Paudyal <benjina.paudyal@gmail.com>
2025-10-29 10:17:51 +0100

    updated about seciton

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +6,24 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const about = document.querySelector('.about-section');
+    if (!about) return; // safety check
 
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            about.classList.add('visible');
+            observer.unobserve(about);
+          }
+        });
+      },
+      { threshold: 0.3 }
+    );
+    observer.observe(about);
+  }
 }
